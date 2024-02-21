7864c53e962188206bc2da6a13b7df3a5d953530
kcwl <767899122@qq.com>
2024-02-21 18:37:49 +0800

    add for each

--- include/reflect.hpp
+++ include/reflect.hpp
@@ -54,6 +54,33 @@ namespace reflect
 	{
 		return std::get<N>(decltype(_Ty::template make_reflect_member<_Ty>())::apply_member());
 	}
+
+	template <typename _Tuple, typename _Func, std::size_t... I>
+	constexpr auto for_each(_Tuple&& tuple, _Func&& f, std::index_sequence<I...>)
+	{
+		return (std::forward<_Func>(f)(get<I>(std::forward<_Tuple>(tuple))), ...);
+	}
+
+	template <typename _Ty, typename _Func>
+	constexpr auto for_each(_Ty&& tp, _Func&& f)
+	{
+		return for_each(std::forward<_Ty>(tp), std::forward<_Func>(f),
+			std::make_index_sequence<tuple_size_v<_Ty>>{});
+	}
+
+	template <typename _Tuple, typename _Func, std::size_t... I>
+	constexpr auto for_each_element(_Tuple&& tuple, _Func&& f, std::index_sequence<I...>)
+	{
+		return (std::forward<_Func>(f)(name<_Tuple, I>(), reflect::get<I>(std::forward<_Tuple>(tuple)), std::move(I)),
+			...);
+	}
+
+	template <typename _Ty, typename _Func>
+	constexpr auto for_each_element(_Ty&& tp, _Func&& f)
+	{
+		return for_each_element(std::forward<_Ty>(tp), std::forward<_Func>(f),
+			std::make_index_sequence<reflect::tuple_size_v<_Ty>>{});
+	}
 }
 
 
